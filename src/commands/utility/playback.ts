74069345e4c11b06eae11945b24a74188dff8293
Veren Villegas <verenvillegas@gmail.com>
2024-05-04 18:33:06 +1200

    add : bot can play the previous song

---
+++ src/commands/utility/playback.ts
@@ -0,0 +1,26 @@
+const { useHistory } = require("discord-player");
+import { GuildQueueHistory } from "discord-player";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("playback")
+    .setDescription("Plays the previous song in the queue, if there is one."),
+  async execute(interaction: ChatInputCommandInteraction) {
+    if (interaction.guild) {
+      if (interaction.guild.id) {
+        const history: GuildQueueHistory = useHistory(interaction.guild.id);
+        if (history) {
+          if (history.previousTrack) {
+            await history.previous();
+            await interaction.reply(
+              `Running it back to the previous song: ${history.previousTrack.title}`
+            );
+          }
+          return;
+        }
+        await interaction.reply(`Playback error: No previous song found.`);
+      }
+    }
+  },
+};

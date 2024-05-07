bcb26594104df19b4790b3d017ffa43219e96f87
Veren Villegas <verenvillegas@gmail.com>
2024-05-07 17:09:56 +1200

    update : skip playlist feature added and improved replies

--- src/commands/utility/skip.ts
+++ src/commands/utility/skip.ts
@@ -1,38 +1,136 @@
-import { GuildQueue } from "discord-player";
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { GuildQueue, Track } from "discord-player";
+import {
+  AutocompleteInteraction,
+  ChatInputCommandInteraction,
+  SlashCommandBuilder,
+  SlashCommandStringOption,
+} from "discord.js";
 
 const { useQueue } = require("discord-player");
 
+function getPlaylists(queue: GuildQueue) {
+  if (!queue || !queue.tracks.size) {
+    return null; // Queue is empty or not found
+  }
+
+  let tracks = queue.tracks.filter((track) => track.playlist != undefined);
+
+  const uniquePlaylistNames: string[] = [];
+  tracks.forEach((track) => {
+    if (track.playlist && !uniquePlaylistNames.includes(track.playlist.title)) {
+      uniquePlaylistNames.push(track.playlist.title);
+    }
+  });
+
+  uniquePlaylistNames.sort();
+
+  return uniquePlaylistNames;
+}
+
+/**
+ * Potential fix: Add the URL of the playlist in the removal comparison so that playlists with the same name will not be deleted.
+ * */
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("skip")
-    .setDescription("Skips the current track."),
+    .setDescription("Skips the current track.")
+    .addStringOption((option: SlashCommandStringOption) =>
+      option
+        .setName("playlist")
+        .setDescription("The name of the playlist to skip.")
+        .setAutocomplete(true)
+    ),
+  async autocomplete(interaction: AutocompleteInteraction) {
+    const focusedValue = interaction.options.getFocused();
+    if (interaction.guild) {
+      const queue: GuildQueue = useQueue(interaction.guild.id);
+      const choices = getPlaylists(queue);
+      if (choices) {
+        const filtered = choices.filter((choice) =>
+          choice.startsWith(focusedValue)
+        );
+        await interaction.respond(
+          filtered.map((choice) => ({ name: choice, value: choice }))
+        );
+      } else await interaction.respond([]);
+    }
+  },
   async execute(interaction: ChatInputCommandInteraction) {
     if (interaction.guild) {
       const queue: GuildQueue = useQueue(interaction.guild.id);
       await interaction.deferReply();
       if (queue && queue.currentTrack) {
         let song = queue.currentTrack;
-        if (song.playlist?.title)
-          console.log(
-            `This song is part of a playlist called ${song.playlist?.title}.`
-          );
-        queue.node.skip();
+        let playlistSkip = interaction.options.getString("playlist");
+        let validPlaylist = true;
+        let isPartPlaylist = false;
+        if (playlistSkip) {
+          const playlists = getPlaylists(queue);
+          if (playlists?.includes("playlistSkip")) {
+            queue.tracks.remove(
+              (track) =>
+                track.playlist != undefined &&
+                track.playlist.title === playlistSkip
+            );
+            console.log(`Tracks left in the queue: ${queue.tracks.size}`);
+            if (song.playlist?.title === playlistSkip) {
+              console.log(
+                `${song.title} is part of a playlist called ${song.playlist?.title}. Skipping song...`
+              );
+              queue.node.skip();
+              isPartPlaylist = true;
+            }
+          } else validPlaylist = false;
+        } else queue.node.skip();
+        let nextSong: Track | undefined;
         if (queue.node.getTrackPosition(song) < queue.size - 1) {
           let nextSongPos = queue.node.getTrackPosition(song) + 1;
-          let nextSong = queue.tracks.at(nextSongPos);
+          nextSong = queue.tracks.at(nextSongPos);
+        }
+        if (!validPlaylist) {
           await interaction.followUp(
-            `**${"Skipped"}**:\n> \`${song}\`\n\n**${"Now playing"}**:\n> \`${
-              nextSong?.title
-            } by ${nextSong?.author}\``
+            `**No playlists were found with the name**:\n> \`${playlistSkip}\``
           );
+        } else if (playlistSkip && isPartPlaylist) {
+          if (nextSong)
+            await interaction.followUp(
+              `**${"Playlist Skipped"}**:\n> \`${
+                song.playlist?.title
+              }\`\n\n**${"Up next"}**:\n> \`${nextSong?.title} by ${
+                nextSong?.author
+              }\``
+            );
+          else
+            await interaction.followUp(
+              `**${"Playlist Skipped"}**:\n > \`${song}\`\n\n**No more songs in the queue.**`
+            );
+        } else if (playlistSkip && !isPartPlaylist) {
+          if (nextSong)
+            await interaction.followUp(
+              `**${"Playlist skipped"}**:\n> \`${playlistSkip}\`\n\n**${"Up next"}**:\n> \`${
+                nextSong.title
+              } by ${nextSong.author}\``
+            );
+          else
+            await interaction.followUp(
+              `**${"Playlist skipped"}**:\n> \`${playlistSkip}\`\n\n*__This is the final song__*`
+            );
         } else {
-          await interaction.followUp(
-            `**${"Skipped"}**:\n > \`${song}\`\n\n**No more songs in the queue.**`
-          );
+          if (nextSong)
+            await interaction.followUp(
+              `**${"Skipped"}**:\n> \`${
+                song.title
+              }\`\n\n**${"Up next"}**:\n> \`${nextSong?.title} by ${
+                nextSong?.author
+              }\``
+            );
+          else
+            await interaction.followUp(
+              `**${"Skipped"}**:\n > \`${song}\`\n\n**No more songs in the queue.**`
+            );
         }
       } else
-        await interaction.reply(
+        await interaction.followUp(
           `**/skip Error**: *There are no songs to skip.*\n> Please play a song first, using \`/play\`.`
         );
     }

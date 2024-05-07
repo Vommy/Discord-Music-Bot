0e979516a384f966535cc2dd6f9875fd9a96faa7
Veren Villegas <verenvillegas@gmail.com>
2024-05-07 17:07:17 +1200

    update : clear now checks for empty queue

--- src/commands/utility/clear.ts
+++ src/commands/utility/clear.ts
@@ -9,8 +9,13 @@ module.exports = {
     if (interaction.guild) {
       if (interaction.guild.id) {
         const queue = useQueue(interaction.guild.id);
-        if (queue) queue.tracks.clear();
-        await interaction.reply(`Song Queue Cleared!`);
+        if (queue) {
+          queue.tracks.clear();
+          await interaction.reply(`**Song Queue Cleared!**`);
+        } else
+          await interaction.reply(
+            `**/clear Error**: *There are no songs to clear.*\n> Please queue or play a song before clearing, then try again.`
+          );
       }
     }
   },
